4bd1462afac50779d0ebab3a5127600ab235a2e2
MKaringa <mungaikaringa1@gmail.com>
2021-04-11 11:49:01 +0300

    Add method slice to extract century and year digits from input year

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ function getDay(name) {
     let date = document.getElementById("day_input").value;
     let month = document.getElementById("month_input").value;
     let year = document.getElementById("year_input").value;
+    let century = year.slice(0,1);
+    let yearsDigits = year.slice(2, 3);
 
 }
