634c8ae70a0db221ad233499d30f6e70d4f38d32
MKaringa <mungaikaringa1@gmail.com>
2021-04-11 12:14:04 +0300

    Check whether user inputs all data correctly then calculate day

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ function getDay(name) {
     let century = year.slice(0,1);
     let yearsDigits = year.slice(2, 3);
 
+    if (year === ''|| year.length> 4 || year.lenght< 4){
+        alert('Enter a valid year');
+    } else if(month === '' || month < 1 || month > 12){
+        alert('Enter a valid month');
+    } else if (date === '' || date < 0 || date > 31){
+        alert('Enter a valid date');
+    } else{
+        let day = Math.floor (((century/4) - 2* century -1 )+ ((5 * yearsDigits/4)) + ((26*(month+1)/10)) + date) % 7;
+    }
 }
+
