a24ad0a07d3863e168c4979922431fa11f8639cf
MKaringa <mungaikaringa1@gmail.com>
2021-04-11 11:44:03 +0300

    Add funtion to get the date

---
+++ index.js
@@ -0,0 +1,6 @@
+function getDay(name) {
+    let date = document.getElementById("day_input").value;
+    let month = document.getElementById("month_input").value;
+    let year = document.getElementById("year_input").value;
+
+}
